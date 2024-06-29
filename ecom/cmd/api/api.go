fa71d855ddd0606e34479378d66a9613009cdfde
Darshan Raul <dar.raul@gmail.com>
2024-06-29 05:53:36 +0000

    tiago course backend api code practice -1

---
+++ ecom/cmd/api/api.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/darshan-raul/ecom/service/user"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db *sql.DB
+}
+
+func NewAPIServer(addr string,db *sql.DB) *APIServer{
+	return &APIServer{
+		addr: addr,
+		db: db,
+	}
+
+}
+
+func (s *APIServer) Run() error{
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr,router)
+
+}
---
+++ ecom/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/darshan-raul/ecom/cmd/api"
+	"github.com/darshan-raul/ecom/config"
+	"github.com/darshan-raul/ecom/db"
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
